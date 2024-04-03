51c86fc0140444e75fddaddcd5f7a2fbf3cafc44
Osim A <g-unit_osim@mail.ru>
2024-04-03 10:15:11 +0500

    cmake for subproject added

---
+++ ProtocolDevelopingTest/HardwarePeripheral/HardwareInterfaceUnit.h
@@ -0,0 +1,10 @@
+#ifndef HARDWARE_INTERFACE_UNIT_H
+#define HARDWARE_INTERFACE_UNIT_H
+
+int Write(void);
+int Recv(void);
+int SentInterrupt(void* arg);
+int ReceiveInterrupt(void* arg);
+
+
+#endif // !HARDWARE_INTERFACE_UNIT_H
